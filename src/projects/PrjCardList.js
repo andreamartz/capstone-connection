403a13b6ffe680ffcae46b78fca43f89dd4c05aa
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-03 16:36:12 -0500

    Add masonry layout to cards on PrjCardList

--- src/projects/PrjCardList.js
+++ src/projects/PrjCardList.js
@@ -1,35 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Masonry from 'react-masonry-css';
+import { Container } from '@material-ui/core';
+import LoadingSpinner from "../common/LoadingSpinner";
 import PrjCardVert from './PrjCardVert';
+import CapConApi from "../api/api";
 import "./PrjCardList.css";
 
 
+function PrjCardList() {
+  
+  const [projects, setProjects] = useState([]);
 
-function PrjCardList({ projects }) {
-  console.debug('PrjCardList', 'projects=', projects);
+  useEffect(() => {
+    getAllProjectsOnMount();
+  }, []); 
+
+  async function getAllProjectsOnMount() {
+    console.debug("ProjectList useEffect getAllProjectsOnMount");
+    const projects = await CapConApi.getProjects();
+    setProjects(projects);
+  }
+
+  if (!projects) return <LoadingSpinner />;
+
+  const breakpoints = {
+    default: 3,  // default number of columns
+    1100: 2,     // at screen width of 1100px, reduce to 2 columns
+    700: 1
+  }
+
   return (
-    <div className="container cards-container border border-secondary-50">
-      <div className="row row-cols-auto" data-masonry='{"percentPosition": true }'>
-        <PrjCardVert>
+    // <div className="cards-container border border-secondary-50">
+    <Container>
 
-        </PrjCardVert>
-        <PrjCardVert>
-
-        </PrjCardVert>
-        <PrjCardVert>
-
-        </PrjCardVert>
-        <PrjCardVert>
-
-        </PrjCardVert>
-        <PrjCardVert>
-
-        </PrjCardVert>
-        <PrjCardVert>
-
-        </PrjCardVert>
-      </div>
-
-    </div>
+      {projects.length 
+        ? (
+        <Masonry
+          breakpointCols={breakpoints}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {projects.map(p => (
+            <PrjCardVert className="card" 
+              key={p.id}
+              name={p.name}
+              firstName={p.firstName}
+              lastName={p.lastName}
+              photoUrl={p.photoUrl}
+              image={p.image}
+              repoUrl={p.repoUrl}
+              siteUrl={p.siteUrl}
+              description={p.description}
+              feedbackRequest={p.feedbackRequest}
+              createdAt={p.createdAt}
+              lastModified={p.lastModified}
+            />
+          ))}
+        </Masonry>
+        ) : (
+          <p className="lead">Sorry, no results were found!</p>
+        )
+      }    
+    </Container>
   );
 }
 
