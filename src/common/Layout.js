ec3ac691bdd063b4db00f4f5e4575314c4410c87
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-06 17:22:41 -0500

    Add Layout component

---
+++ src/common/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => {
+  return {
+    page: {
+      width: '100%'
+    },
+    root: {
+      display: 'flex'
+    }
+  }
+});
+
+function Layout({ children }) {
+  const classes = useStyles();
+
+  return (
+    <div className={ classes.root }>
+      <div className={ classes.page }>
+        { children } 
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
