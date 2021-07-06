f12970551ed1e790af3e4225a0760731b0bd7747
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-06 18:20:13 -0500

    Add theme and ThemeProvider to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider  } from "@material-ui/core";
+import theme from "./common/theme";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
