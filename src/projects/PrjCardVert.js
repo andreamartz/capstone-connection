8c14ef0bc33007cda06a123f3984ffce46761414
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-16 14:17:16 -0500

    Remove unnecessary dependencies

--- src/projects/PrjCardVert.js
+++ src/projects/PrjCardVert.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActions from '@material-ui/core/CardActions';
-import Chip from '@material-ui/core/Chip';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import { Avatar, IconButton, Typography } from '@material-ui/core';
