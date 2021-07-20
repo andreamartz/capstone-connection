81c45fa84c349d39b9c753fcdd25ecc772faa461
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-20 16:03:26 -0500

    Fine tune layout of User Detail Page

--- src/users/UserDetailPage.js
+++ src/users/UserDetailPage.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Masonry from 'react-masonry-css';
 import clsx from 'clsx';
 import { useParams } from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
@@ -7,73 +6,50 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import IconButton from '@material-ui/core/IconButton';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import Link from '@material-ui/core/Link';
 import PrjCardList from '../projects/PrjCardList';
 import { makeStyles } from '@material-ui/core/styles';
 import CapConApi from "../api/api";
 import LoadingSpinner from "../common/LoadingSpinner";
-import PrjCardVert from '../projects/PrjCardVert';
 import "./UserDetailPage.css";
 
 const useStyles = makeStyles((theme) => ({
-  // hero: {
-  //   display: 'flex',
-  //   flexDirection: 'column',
-  //   marginTop:'4rem',
-  //   paddingTop: theme.spacing(14),
-  //   paddingBottom: theme.spacing(14),
-  //   justifyContent: 'center',
-  //   alignItems: 'center'
-  // },
-  // heroTypography: {
-  //   color: '#fff'
-  // },
-  // creatorTypography: {
-  //   fontWeight: 'bold'
-  // },
-  // title: {
-  //   marginBottom: theme.spacing(10)
-  // },
-  // button: {
-  //   borderRadius: '1.2rem'
-  // },
-  // container: {
-  //   paddingLeft: '0',
-  //   paddingRight: '0',
-  //   maxWidth: 'lg',
-  //   margin: 'auto'
-  // },
-  // heroAvatar: {
-  //   marginLeft: theme.spacing(3),
-  //   marginRight: theme.spacing(2)
-  // },
-  // iconButton: {
-  //   padding: '0',
-  //   color: '##000',
-  //   // opacity: 0.87
-  // },
-  // large: {
-  //   width: theme.spacing(6),
-  //   height: theme.spacing(6),
-  // },
-  // like: {
-  //   backgroundColor: "e0e0e0",
-  //   display: 'flex',
-  //   alignItems: 'center',
-  // }
+  hero: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop:'4rem',
+    paddingTop: theme.spacing(12),
+    paddingBottom: theme.spacing(12),
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  heroTypography: {
+    color: '#fff'
+  },
+  title: {
+    marginBottom: theme.spacing(8)
+  },
+  button: {
+    borderRadius: '1.2rem',
+    marginRight: theme.spacing(2),
+    marginLeft: theme.spacing(2)
+  },
+  heroAvatar: {
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+  },
+  large: {
+    width: theme.spacing(15),
+    height: theme.spacing(15),
+  }
 }));
 
 const UserDetailPage = () => {
-  
   const { username } = useParams();
   console.debug("UserDetailPage", "username=", username);
 
   const [user, setUser] = useState(null);
-  const [projects, setProjects] = useState(null);
-
-  const classes = useStyles();
 
   useEffect(() => {
     async function getUserOnMount() {
@@ -86,55 +62,109 @@ const UserDetailPage = () => {
     getUserOnMount();
   }, [username]); 
 
-
-
-  // async function likeProject(projectIdx) {
-  //   const likerId = 4;  // CHECK replace likerId with currentUser.id once we have auth
-  //   const projectId = user.projects[projectIdx].id;
-  //   const like = await CapConApi.addProjectLike({likerId, projectId});  // CHECK replace likerId with currentUser.id once we have auth
-  //   console.log(projectIdx);
-  //   console.log("LIKE: ", like);
-  //   if (like) {
-  //     setUser({...user, projects: [...user.projects, user.projects[projectIdx].prjLikesCount++]});
-  //   }
-  // }
+  const classes = useStyles();
 
   if (!user) return <LoadingSpinner />;
 
-  const breakpoints = {
-    default: 3,  // default number of columns
-    960: 2,     // at screen width of 1100px, reduce to 2 columns
-    600: 1
-  }
-
+  // const breakpoints = {
+  //   default: 3,  // default number of columns
+  //   960: 2,     // at screen width of 1100px, reduce to 2 columns
+  //   600: 1
+  // }
 
   return (
-    <Container>
-      <PrjCardList username={username}/>
-      
-      {/* {user.projects.length 
-        ? (
-        <Masonry
-          breakpointCols={breakpoints}
-          className="masonry-grid"
-          columnClassName="masonry-grid_column"
+    <>
+      <Box 
+        className={classes.hero} 
+        style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.75), rgba(0,0,0,0.75)), url(https://res.cloudinary.com/wahmof2/image/upload/v1626811212/Untitled_design.png)`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'top center' 
+        }}
+      >
+        <Box 
+          display="flex" 
+          alignItems="center"
+          mb={6}
         >
-          {user.projects.map((project, idx) => (
-            <div key={project.id} className={classes.project}>
-              <PrjCardVert
-                project={project}
-                // likeProject={() => {likeProject(idx)}}
-              />
-            </div>
-          ))}
-        </Masonry>
-        ) : (
-          <p>Sorry, no results were found!</p>
-        )
-      }     */}
-    </Container>
+          <Avatar
+            src={user.photoUrl}
+            className={clsx(classes.large, classes.heroAvatar)}
+            alt="project creator"
+          />
+          <Typography 
+            component="h1" 
+            variant="h3"
+            // className={clsx(classes.heroTypography, classes.title)}
+            className={classes.heroTypography}
+          >
+            {user.firstName} {user.lastName}
+          </Typography>
+        </Box>
+
+        <Typography 
+          component="h2" 
+          variant="h5"
+          className={clsx(classes.heroTypography, classes.title)}
+        >
+          {user.bio}
+        </Typography>
+        <Box
+          className={classes.heroTypography}
+          display='flex'
+          justifyContent='space-between'
+          width='60%'
+          maxWidth="710px"
+        >
+          <Box
+            display='flex'
+            justifyContent='center'
+            width='50%'
+            mx='auto'
+          >
+            <Link href={user.portfolioUrl} target="_blank" rel="noopener">
+              <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className={classes.button}
+                endIcon={<OpenInNewIcon />}  
+              >
+                Portfolio
+              </Button>
+            </Link>
+            <Link href={user.gitHubUrl} target="_blank" rel="noopener">
+              <Button
+                variant="contained"
+                size="large"
+                className={classes.button}
+                endIcon={<OpenInNewIcon />}  
+              >
+                GitHub
+              </Button>
+            </Link>
+          </Box>
+
+        </Box>
+      </Box>
+      <Container>
+        <Typography
+          component="h3"
+          variant="h4"
+        >
+          <Box 
+            textAlign="center" 
+            mx="auto" 
+            pt={10}
+            pb={8}
+          >
+            PROJECTS
+          </Box>
+        </Typography>
+
+        <PrjCardList username={username}/>
+      </Container>
+    </>
   );
 }
  
 export default UserDetailPage;
-
