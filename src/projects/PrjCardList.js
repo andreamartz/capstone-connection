2e8ad036e59dfd6466e99977b76f0442220f7ba5
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-07-23 22:18:34 -0500

    Fix bugs with liking and unliking projects in PrjCardList.js

--- src/projects/PrjCardList.js
+++ src/projects/PrjCardList.js
@@ -7,6 +7,16 @@ import CapConApi from "../api/api";
 import { makeStyles } from '@material-ui/core/styles';
 import "./PrjCardList.css";
 
+// See Springboard React Video #6 Forms-Passing Data Up
+// CHECK:
+// 1. Add state for like counts on all projects (with an id for each like?/project?).
+// 2. Create a function that will:
+//    a. update the database with another like for the appropriate object.
+//    b. check if the database update was successful, and, if so,...
+//    c. update the likes state.
+// 3. Pass that function down to PrjCardVert
+// 4. Find a way to UNlike a project
+
 const useStyles = makeStyles((theme) => ({
   project: {
     marginTop: theme.spacing(3),
@@ -14,31 +24,64 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const PrjCardList = () => {
+const PrjCardList = ({ username }) => {
   console.debug("PrjCardList");
 
   const [projects, setProjects] = useState([]);
   const classes = useStyles();
-  console.log("HEY! PROJECTS: ", projects);
 
   useEffect(() => {
+    async function getAllProjectsOnMount() {
+      console.debug("PrjCardList useEffect getAllProjectsOnMount");
+      const projects = await CapConApi.getProjects({ username });
+
+      setProjects(projects);
+    }
+
     getAllProjectsOnMount();
-  }, []); 
+  }, [username]); 
 
-  async function getAllProjectsOnMount() {
-    console.debug("PrjCardList useEffect getAllProjectsOnMount");
-    const projects = await CapConApi.getProjects();
-    setProjects(projects);
-  }
 
-  async function likeProject(projectIdx) {
-    const likerId = 1;  // CHECK replace likerId with currentUser.id once we have auth
-    const projectId = projects[projectIdx].id;
-    const like = await CapConApi.addProjectLike({likerId, projectId});  // CHECK replace likerId with currentUser.id once we have auth
-    console.log(projectIdx);
-    console.log("LIKE: ", like);
-    if (like) {
-      setProjects([...projects, projects[projectIdx].prjLikesCount++]);
+  async function toggleLikeProject(projectIdx) {
+    console.log("PROJECT INDEX: ", projectIdx);
+    const currentUserId = 3;  // CHECK replace currentUserId with currentUser.id once we have auth
+    const likerId = currentUserId;  // CHECK replace likerId with currentUser.id once we have auth
+    const project = projects[projectIdx];
+    console.log("PROJECT: ", project);
+    let { id, likesCount, currentUsersLikeId } = project;
+    const projectId = id;
+
+    // if project already liked by currentUser, unlike it
+    if (currentUsersLikeId) {
+      const unlike = await CapConApi.removeProjectLike(currentUsersLikeId);
+      console.log("UNLIKE: ", unlike);
+      if (unlike) {
+        // setProjects([...projects, {...project, likesCount: likesCount--, currentUsersLikeId: null}]);
+        // setProjects([...projects, {...projects[projectIdx], likesCount: likesCount-1, currentUsersLikeId: null}]);
+        setProjects(currentProjects => {
+          const newProjects = [...currentProjects];
+          newProjects[projectIdx] = {...newProjects[projectIdx], likesCount: likesCount-1, currentUsersLikeId: null};
+
+          return newProjects;
+          
+        });
+      }
+    } else {
+      // otherwise, like it
+      const like = await CapConApi.addProjectLike({projectId, likerId });  // CHECK replace likerId with currentUser.id once we have auth
+      console.log("LIKE: ", like);
+      if (like) {
+        // setProjects([...projects, {...project, likesCount: likesCount++, currentUsersLikeId: like.id}]);
+        // setProjects([...projects, {...projects[projectIdx], likesCount: likesCount+1, currentUsersLikeId: like.id}]);
+        setProjects(currentProjects => {
+          const newProjects = [...currentProjects];
+          newProjects[projectIdx] = {...newProjects[projectIdx], likesCount: likesCount+1, currentUsersLikeId: like.id};
+
+          return newProjects;
+
+          // return [...currentProjects, {...currentProjects[projectIdx], likesCount: likesCount+1, currentUsersLikeId: like.id}]
+        });
+      }
     }
   }
 
@@ -50,26 +93,27 @@ const PrjCardList = () => {
     600: 1
   }
 
+  console.log("PROJECTS STATE: ", projects);
+  
   return (
-    // <div className="cards-container border border-secondary-50">
     <Container>
 
       {projects.length 
         ? (
-        <Masonry
-          breakpointCols={breakpoints}
-          className="masonry-grid"
-          columnClassName="masonry-grid_column"
-        >
-          {projects.map((p, idx) => (
-            <div key={p.id} className={classes.project}>
-              <PrjCardVert
-                project={p}
-                likeProject={() => {likeProject(idx)}}
-              />
-            </div>
-          ))}
-        </Masonry>
+          <Masonry
+            breakpointCols={breakpoints}
+            className="masonry-grid"
+            columnClassName="masonry-grid_column"
+          >
+            {projects.map((p, idx) => (
+              <div key={p.id} className={classes.project}>
+                <PrjCardVert
+                  project={p}
+                  toggleLikeProject={() => {toggleLikeProject(idx)}}
+                />
+              </div>
+            ))}
+          </Masonry>
         ) : (
           <p>Sorry, no results were found!</p>
         )
