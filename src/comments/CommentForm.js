33c0782863060847af251cf0378a2930410cdea5
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-05 14:12:01 -0500

    Remove unused useEffect dependency

--- src/comments/CommentForm.js
+++ src/comments/CommentForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { useHistory } from "react-router-dom";
 import Avatar from '@material-ui/core/Avatar';
 import Box from '@material-ui/core/Box';
