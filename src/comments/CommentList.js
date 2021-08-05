dc2df4d67c3fa378676f42d0eed8fae9372188ec
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-05 15:52:18 -0500

    Remove unnecessary imports

--- src/comments/CommentList.js
+++ src/comments/CommentList.js
@@ -1,11 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import { List } from "@material-ui/core";
 import Comment from "./Comment";
-import CapConApi from "../api/api";
-
 
 const useStyles = makeStyles(theme => ({
   root: {
