3afb2162fb4296327b27e15cde8dff13955500e3
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-07 16:56:24 -0500

    Remove unnecessary imports from search form

--- src/common/SearchForm.js
+++ src/common/SearchForm.js
@@ -1,14 +1,8 @@
 import React, { useState } from "react";
-import Alert from '@material-ui/lab/Alert';
 import { useHistory } from 'react-router-dom';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
