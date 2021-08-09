7ff86ea4edc676110cb3108cbe7127a5862c3706
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-09 17:31:10 -0500

    Revamp Homepage.js

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,8 +1,40 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import "./Homepage.css";
 import UserContext from "../auth/UserContext";
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { classes } from "istanbul-lib-coverage";
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    marginTop: theme.spacing(8),
+    height: 'calc(100vh - 150px)'
+  },
+  gridContainer: {
+    marginTop: theme.spacing(8),
+    paddingTop: theme.spacing(15),
+    alignItems: 'center',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  gridHeading: {
+    fontWeight: 'bold',
+    fontSize: theme.spacing(6),
+    color: 'primary',
+    paddingBottom: theme.spacing(2)
+  },
+  gridItem: {
+    fontSize: theme.spacing(3),
+    paddingBottom: theme.spacing(6)
+  },
+  welcome: {
+    color: '#ff9c2a',
+    fontFamily: 'Roboto Mono, sans',
+    fontSize: theme.spacing(3),
+  }
+}));
 /** Homepage of site.
  *
  * Shows welcome message or login/register buttons.
@@ -15,32 +47,41 @@ import UserContext from "../auth/UserContext";
 function Homepage() {
   const { currentUser } = useContext(UserContext);
   console.debug("Homepage", "currentUser=", currentUser);
+  const classes = useStyles();
 
   return (
-    <div className="Homepage">
-      <div className="container text-center">
-        <h1 className="mb-4 font-weight-bold">Capstone Connections</h1>
-        <p className="lead">The community that connects with and supports you!</p>
-        {/* CHECK: uncomment once we have auth */}
-        {/* {currentUser
-          ? <h2>
-            Welcome Back, {currentUser.firstName || currentUser.username}!
-          </h2>
-          : (
-              <p>
-                <Link className="btn btn-primary font-weight-bold mr-3"
-                      to="/login">
-                  Log in
-                </Link>
-                <Link className="btn btn-primary font-weight-bold"
-                      to="/signup">
-                  Sign up
-                </Link>
-              </p>
-          )
-        } */}
-      </div>
-    </div>
+    <Container className={classes.container}>
+      <Grid container className={classes.gridContainer}>
+        <Grid item>
+          <Typography 
+            className={classes.gridHeading} 
+            component="h1"
+            color='primary'
+          >
+            Capstone Connections
+          </Typography>
+        </Grid>
+        <Grid>
+          <Typography 
+            className={classes.gridItem} 
+            component="h1"
+          >
+            The community that connects with and supports you!
+          </Typography>
+        </Grid>
+        <Grid>
+          <Typography 
+            className={classes.welcome}
+          >
+            {currentUser && 
+              <h2>
+                Welcome Back, {currentUser.firstName || currentUser.username}!
+              </h2>
+            }
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
