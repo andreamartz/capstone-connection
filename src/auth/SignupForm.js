e3ed797ce2fc2875d26f19d9f12f4cb773648a76
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-10 17:27:57 -0500

    Update imports

--- src/auth/SignupForm.js
+++ src/auth/SignupForm.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef } from 'react';
-import { Formik, Field, Form, useFormik } from 'formik';
+import {  useFormik } from 'formik';
 import { useHistory } from "react-router-dom";
-import CapConApi from "../api/api";
 import Avatar from '@material-ui/core/Avatar';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
@@ -10,7 +9,6 @@ import AddCircleOutlineOutlinedIcon from '@material-ui/icons/Add';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import './SignupForm.css';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
