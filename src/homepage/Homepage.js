7788bab5b1696a61f6131dd041f5a7bdeabd091b
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-10 16:57:14 -0500

    Update imports

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,11 +1,11 @@
 import React, { useContext } from "react";
-import { Link } from "react-router-dom";
+
 import UserContext from "../auth/UserContext";
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { classes } from "istanbul-lib-coverage";
+import { makeStyles } from '@material-ui/core/styles';
+
 
 const useStyles = makeStyles(theme => ({
   container: {
