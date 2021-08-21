cdc7246ce5dff79f9e267a7452b47e0c263150a5
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-20 22:25:19 -0500

    Add Page Not Found component

---
+++ src/common/PageNotFound404.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  message: {
+    margin: '200px auto 0 auto',
+    color: 'tomato',
+    fontSize: theme.spacing(3),
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+}));
+
+const PageNotFound404 = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.message}>
+      Page Not Found
+    </div>
+  );
+}
+
+export default PageNotFound404;
+
