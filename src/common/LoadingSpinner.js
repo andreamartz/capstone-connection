52aa166b9063239ad4f071a9d61f2c6b915ff170
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-08-20 22:16:18 -0500

    Move Loding Spinner styles out of css and into the component file

--- src/common/LoadingSpinner.js
+++ src/common/LoadingSpinner.js
@@ -1,13 +1,24 @@
 import React from "react";
-import "./LoadingSpinner.css";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  message: {
+    margin: '200px auto 0 auto',
+    color: 'tomato',
+    fontSize: theme.spacing(3),
+    fontWeight: 'bold',
+    textAlign: 'center'
+  }
+}));
 
 /** Loading message used by components that fetch API data. */
 
 function LoadingSpinner() {
+  const classes = useStyles();
   return (
-      <div className="LoadingSpinner">
-        Loading ...
-      </div>
+    <div className={classes.message}>
+      Loading ...
+    </div>
   );
 }
 
