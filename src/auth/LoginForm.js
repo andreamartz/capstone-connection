82314d70372f7c21f4705d1452623d42ff2b8192
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-02 18:33:15 -0500

    Improve code formatting in LoginForm.js

--- src/auth/LoginForm.js
+++ src/auth/LoginForm.js
@@ -11,134 +11,137 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(16),
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    paddingTop: theme.spacing(8),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    height: '60vh',
-    width: '22rem',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.warning.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+	paper: {
+		marginTop: theme.spacing(16),
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		paddingTop: theme.spacing(8),
+		paddingLeft: theme.spacing(2),
+		paddingRight: theme.spacing(2),
+		display: 'flex',
+		flexDirection: 'column',
+		height: '60vh',
+		width: '22rem',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.warning.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	submit: {
+		margin: theme.spacing(3, 0, 2),
+	},
 }));
 
 const LoginForm = ({ login }) => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: ""
-  });
-  const [formErrors, setFormErrors] = useState([]);
-  
-  console.debug(
-    "LoginForm",
-    "login=", typeof login,
-    "formData=", formData,
-    "formErrors", formErrors,
-  );
+	const [formData, setFormData] = useState({
+		username: '',
+		password: '',
+	});
+	const [formErrors, setFormErrors] = useState([]);
 
-  const classes = useStyles();
-  const history = useHistory();
+	console.debug(
+		'LoginForm',
+		'login=',
+		typeof login,
+		'formData=',
+		formData,
+		'formErrors',
+		formErrors,
+	);
 
-  /** Handle form submit:
-   * 
-   * Calls login function prop and, if successful, redirect to /projects.
-   */
+	const classes = useStyles();
+	const history = useHistory();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const result = await login(formData);
-    if (result.success) {
-      history.push("/projects");
-    } else {
-      setFormErrors(result.errors);
-    }
-  }
+	/** Handle form submit:
+	 *
+	 * Calls login function prop and, if successful, redirect to /projects.
+	 */
 
-  /** Update form data field */
-  const handleChange = async (e) => {
-    const { name, value } = e.target;
-    setFormData(data => ({ ...data, [name]: value }));
-  }
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const result = await login(formData);
+		if (result.success) {
+			history.push('/projects');
+		} else {
+			setFormErrors(result.errors);
+		}
+	};
 
-  return (
-    <Paper className={classes.paper} elevation={5} component="main" maxwidth="xs">
-      <Avatar className={classes.avatar}>
-        <LockOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Sign in
-      </Typography>
-      <form className={classes.form} 
-        onSubmit={handleSubmit} 
-      >
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="username"
-          label="Username"
-          name="username"
-          onChange={handleChange}
-          type="text"
-          value={formData.username}
-          autoComplete="username"
-          autoFocus
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          onChange={handleChange}
-          type="password"
-          value={formData.password}
-        />
-        {/* {formErrors.length
+	/** Update form data field */
+	const handleChange = async (e) => {
+		const { name, value } = e.target;
+		setFormData((data) => ({ ...data, [name]: value }));
+	};
+
+	return (
+		<Paper
+			className={classes.paper}
+			elevation={5}
+			component="main"
+			maxwidth="xs"
+		>
+			<Avatar className={classes.avatar}>
+				<LockOutlinedIcon />
+			</Avatar>
+			<Typography component="h1" variant="h5">
+				Sign in
+			</Typography>
+			<form className={classes.form} onSubmit={handleSubmit}>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="username"
+					label="Username"
+					name="username"
+					onChange={handleChange}
+					type="text"
+					value={formData.username}
+					autoComplete="username"
+					autoFocus
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="password"
+					name="password"
+					label="Password"
+					onChange={handleChange}
+					type="password"
+					value={formData.password}
+				/>
+				{/* {formErrors.length
           ? <Alert severity="error">This is an error alert — check it out!</Alert>
           : null
         } */}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          size="large"
-        >
-          Sign In
-        </Button>
-        <Grid container>
-          <Grid item>
-            {"Don't have an account? "}
-            <Link href="/signup">
-              {"Sign Up"}
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
-    </Paper>
-  );
-}
+				<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					color="primary"
+					className={classes.submit}
+					size="large"
+				>
+					Sign In
+				</Button>
+				<Grid container>
+					<Grid item>
+						{"Don't have an account? "}
+						<Link href="/signup">{'Sign Up'}</Link>
+					</Grid>
+				</Grid>
+			</form>
+		</Paper>
+	);
+};
 
 export default LoginForm;
