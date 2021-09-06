5898dcdea8d8270c4cee9df67ebcfb90773aa6c1
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 16:26:02 -0500

    Clean up code formatting

--- src/common/AlertDisplay.test.js
+++ src/common/AlertDisplay.test.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import AlertDisplay from "./AlertDisplay";
-import { MemoryRouter } from "react-router";
+import React from 'react';
+import { render } from '@testing-library/react';
+import AlertDisplay from './AlertDisplay';
+import { MemoryRouter } from 'react-router';
 
-describe("Smoke test", () => {
+describe('Smoke test', () => {
   test('it renders without crashing', () => {
-    render(<AlertDisplay />)
+    render(<AlertDisplay />);
   });
 });
