073d985da827e19c70c7e797215113cdde214e29
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 15:49:41 -0500

    Clean up code formatting

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import Layout from './common/Layout';
-import useLocalStorage from "./hooks/useLocalStorage";
-import Navigation from "./routes-nav/Navigation";
-import Routes from "./routes-nav/Routes";
-import CapConApi from "./api/api";
-import LoadingSpinner from "./common/LoadingSpinner";
-import UserContext from "./auth/UserContext";
-import jwt from "jsonwebtoken";
+import useLocalStorage from './hooks/useLocalStorage';
+import Navigation from './routes-nav/Navigation';
+import Routes from './routes-nav/Routes';
+import CapConApi from './api/api';
+import LoadingSpinner from './common/LoadingSpinner';
+import UserContext from './auth/UserContext';
+import jwt from 'jsonwebtoken';
 import './App.css';
 
-
 // Key name for storing token in localStorage for "remember me" re-login
-export const TOKEN_STORAGE_ID = "capstone-connections-token";
-
+export const TOKEN_STORAGE_ID = 'capstone-connections-token';
 
 /** Capstone Connections application
- * 
+ *
  * - infoLoaded: has user data been pulled from API?
  *   (this manages spinner for "loading...")
  *
@@ -36,47 +34,53 @@ function App() {
   const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
 
   console.debug(
-    "App",
-    "infoLoaded=", infoLoaded,
-    "currentUser=", currentUser,
-    "token=", token,
+    'App',
+    'infoLoaded=',
+    infoLoaded,
+    'currentUser=',
+    currentUser,
+    'token=',
+    token
   );
 
   // Load user info from API. Until a user is logged in and they have a token,
   // this should not run. It only needs to re-run when a user logs out, so
   // the value of the token is a dependency for this effect.
 
-  useEffect(function loadUserInfo() {
-    async function getCurrentUser() {
-      if (token) {
-        try {
-          console.log("INSIDE GETCURRENT USER. TOKEN: ", token);
-          const { id } = jwt.decode(token);
-          // put the token on the Api class so it can use it to call the API.
-          CapConApi.token = token;
-          let currentUser = await CapConApi.getUser(id);
-          console.log("App", "CURRENTUSER: ", currentUser, "TOKEN: ", token);
-          setCurrentUser(currentUser);
-        } catch (err) {
-          console.error("App loadUserInfo: problem loading", err);
-          setCurrentUser(null);
+  useEffect(
+    function loadUserInfo() {
+      async function getCurrentUser() {
+        if (token) {
+          try {
+            console.log('INSIDE GETCURRENT USER. TOKEN: ', token);
+            const { id } = jwt.decode(token);
+            // put the token on the Api class so it can use it to call the API.
+            CapConApi.token = token;
+            let currentUser = await CapConApi.getUser(id);
+            console.log('App', 'CURRENTUSER: ', currentUser, 'TOKEN: ', token);
+            setCurrentUser(currentUser);
+          } catch (err) {
+            console.error('App loadUserInfo: problem loading', err);
+            setCurrentUser(null);
+          }
         }
+        setInfoLoaded(true);
       }
-      setInfoLoaded(true);
-    }
 
-    // set infoLoaded to false while async getCurrentUser runs; once the
-    // data is fetched (or even if an error happens!), this will be set back
-    // to true to control the spinner.
-    setInfoLoaded(false);
-    getCurrentUser();
-  }, [token]);
+      // set infoLoaded to false while async getCurrentUser runs; once the
+      // data is fetched (or even if an error happens!), this will be set back
+      // to true to control the spinner.
+      setInfoLoaded(false);
+      getCurrentUser();
+    },
+    [token]
+  );
 
   /** Handles site-wide logout. */
   function logout() {
-    console.log("Logged in CURRENTUSER: ", currentUser);
+    console.log('Logged in CURRENTUSER: ', currentUser);
     setCurrentUser(null);
-    console.log("Logged out CURRENTUSER: ", currentUser);
+    console.log('Logged out CURRENTUSER: ', currentUser);
     setToken(null);
   }
 
@@ -93,7 +97,10 @@ function App() {
       setToken(token);
       return { success: true };
     } catch (errors) {
-      console.error("signup failed", errors);
+      console.error('signup failed', errors);
+      return { success: false, errors };
+    }
+  }
       return { success: false, errors };
     }
   }
@@ -108,7 +115,7 @@ function App() {
       setToken(token);
       return { success: true };
     } catch (errors) {
-      console.error("login failed", errors);
+      console.error('login failed', errors);
       return { success: false, errors };
     }
   }
@@ -117,12 +124,10 @@ function App() {
 
   return (
     <Layout>
-      <UserContext.Provider
-        value={{ currentUser, setCurrentUser }}
-      >
+      <UserContext.Provider value={{ currentUser, setCurrentUser }}>
         <div className="App">
           <Navigation logout={logout} />
-          <Routes login={login} signup={signup} />
+          <Routes login={login} signup={signup} signupDemo={signupDemo} />
         </div>
       </UserContext.Provider>
     </Layout>
