a86bcb74933ac253e9955ed0971750ba0d32e23a
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 16:25:39 -0500

    Clean up code formatting

--- src/common/AlertDisplay.js
+++ src/common/AlertDisplay.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import Alert from '@material-ui/lab/Alert';
 
 /** Presentational component for showing bootstrap-style alerts.
@@ -6,22 +6,18 @@ import Alert from '@material-ui/lab/Alert';
  * { LoginForm, SignupForm, ProfileForm } -> Alert
  **/
 
-const AlertDisplay = ({ severity = "error", messages = [] }) => {
-  console.debug("Alert", "severity=", severity, "messages=", messages);
+const AlertDisplay = ({ severity = 'error', messages = [] }) => {
+  console.debug('Alert', 'severity=', severity, 'messages=', messages);
 
   return (
     <div>
-      {messages.map(error => (
-        <Alert
-          variant="outlined"
-          severity="error"
-          key={error}
-        >
+      {messages.map((error) => (
+        <Alert variant="outlined" severity="error" key={error}>
           {error}
         </Alert>
       ))}
     </div>
   );
-}
+};
 
 export default AlertDisplay;
