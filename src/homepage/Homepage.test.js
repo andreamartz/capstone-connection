0c14e6e0fa621ab3347c3d5d6f3309a5d2cd6e7e
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 16:29:32 -0500

    Clean up code formatting

--- src/homepage/Homepage.test.js
+++ src/homepage/Homepage.test.js
@@ -1,15 +1,17 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Homepage from "./Homepage";
-import { MemoryRouter } from "react-router";
-import { UserProvider } from "../testUtils";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Homepage from './Homepage';
+import { MemoryRouter } from 'react-router';
+import { UserProvider } from '../testUtils';
 
-describe("Smoke test", () => {
+describe('Smoke test', () => {
   test('it renders without crashing', () => {
-    render(<MemoryRouter>
-      <UserProvider>
-        <Homepage />
-      </UserProvider>
-    </MemoryRouter>)
+    render(
+      <MemoryRouter>
+        <UserProvider>
+          <Homepage />
+        </UserProvider>
+      </MemoryRouter>
+    );
   });
 });
