7d0d9a5bf72f0cdf5fe5d1fd8bcf6464511a0c42
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 15:29:34 -0500

    Clean up code formatting

--- src/homepage/Homepage.js
+++ src/homepage/Homepage.js
@@ -1,39 +1,38 @@
-import React, { useContext } from "react";
-
-import UserContext from "../auth/UserContext";
+import React, { useContext } from 'react';
+import UserContext from '../auth/UserContext';
+import CapConApi from '../api/api';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   container: {
     marginTop: theme.spacing(8),
-    height: 'calc(100vh - 150px)'
+    height: 'calc(100vh - 150px)',
   },
   gridContainer: {
     marginTop: theme.spacing(8),
     paddingTop: theme.spacing(15),
     alignItems: 'center',
     flexDirection: 'column',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
   gridHeading: {
     fontWeight: 'bold',
     fontSize: theme.spacing(6),
     color: 'primary',
-    paddingBottom: theme.spacing(2)
+    paddingBottom: theme.spacing(2),
   },
   gridItem: {
     fontSize: theme.spacing(3),
-    paddingBottom: theme.spacing(6)
+    paddingBottom: theme.spacing(6),
   },
   welcome: {
     color: '#ff9c2a',
     fontFamily: 'Roboto Mono, sans',
     fontSize: theme.spacing(3),
-  }
+  },
 }));
 /** Homepage of site.
  *
@@ -46,38 +45,33 @@ const useStyles = makeStyles(theme => ({
 
 function Homepage() {
   const { currentUser } = useContext(UserContext);
-  console.debug("Homepage", "currentUser=", currentUser);
+  console.debug('Homepage', 'currentUser=', currentUser);
   const classes = useStyles();
 
   return (
     <Container className={classes.container}>
       <Grid container className={classes.gridContainer}>
         <Grid item>
-          <Typography 
-            className={classes.gridHeading} 
+          <Typography
+            className={classes.gridHeading}
             component="h1"
-            color='primary'
+            color="primary"
           >
             Capstone Connections
           </Typography>
         </Grid>
-        <Grid>
-          <Typography 
-            className={classes.gridItem} 
-            component="h1"
-          >
+        <Grid item>
+          <Typography className={classes.gridItem} component="h1">
             The community that connects with and supports you!
           </Typography>
         </Grid>
-        <Grid>
-          <Typography 
-            className={classes.welcome}
-          >
-            {currentUser && 
+        <Grid item>
+          <Typography className={classes.welcome}>
+            {currentUser && (
               <h2>
                 Welcome Back, {currentUser.firstName || currentUser.username}!
               </h2>
-            }
+            )}
           </Typography>
         </Grid>
       </Grid>
