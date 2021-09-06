637ff7aabd544234dadba011e721933950004f3b
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 16:25:00 -0500

    Clean up code formatting

--- src/comments/EditCommentForm.test.js
+++ src/comments/EditCommentForm.test.js
@@ -1,30 +1,31 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import EditCommentForm from "./EditCommentForm";
-import { MemoryRouter } from "react-router";
-import { UserProvider } from "../testUtils";
+import React from 'react';
+import { render } from '@testing-library/react';
+import EditCommentForm from './EditCommentForm';
+import { MemoryRouter } from 'react-router';
+import { UserProvider } from '../testUtils';
 
 const projectId = 1;
 const idx = 1;
 const comment = {
   id: 1,
-  comment: "Comment with id 1 by user 1",
-  commentCreatedAt: "Created At Time Stamp",
-  commentLastModified: "Last Modified Time Stamp",
+  comment: 'Comment with id 1 by user 1',
+  commentCreatedAt: 'Created At Time Stamp',
+  commentLastModified: 'Last Modified Time Stamp',
   commenter: {
     id: 1,
-    firstName: "Commenter First Name",
-    lastName: "Commenter Last Name",
-    photoUrl: "url@url.comhttps://pbs.twimg.com/profile_images/770491761412173826/ZUeIa4tw_400x400.jpg"
-  }
+    firstName: 'Commenter First Name',
+    lastName: 'Commenter Last Name',
+    photoUrl:
+      'url@url.comhttps://pbs.twimg.com/profile_images/770491761412173826/ZUeIa4tw_400x400.jpg',
+  },
 };
 
-describe("Smoke test", () => {
+describe('Smoke test', () => {
   test('it renders without crashing', () => {
     render(
       <MemoryRouter>
         <UserProvider>
-          <EditCommentForm 
+          <EditCommentForm
             commentState={comment}
             projectId={projectId}
             idx={idx}
