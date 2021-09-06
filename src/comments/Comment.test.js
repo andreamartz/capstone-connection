3fe39aed8c4ec4e71419c1b0d0b9c916af36ad15
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 16:21:19 -0500

    Clean up code formatting

--- src/comments/Comment.test.js
+++ src/comments/Comment.test.js
@@ -1,34 +1,31 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Comment from "./Comment";
-import { MemoryRouter } from "react-router";
-import { UserProvider } from "../testUtils";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Comment from './Comment';
+import { MemoryRouter } from 'react-router';
+import { UserProvider } from '../testUtils';
 
 const comment = {
   id: 1,
-  comment: "Comment with id 1 by user 1",
-  commentCreatedAt: "Created At Time Stamp",
-  commentLastModified: "Last Modified Time Stamp",
+  comment: 'Comment with id 1 by user 1',
+  commentCreatedAt: 'Created At Time Stamp',
+  commentLastModified: 'Last Modified Time Stamp',
   commenter: {
     id: 1,
-    firstName: "Commenter First Name",
-    lastName: "Commenter Last Name",
-    photoUrl: "url@url.comhttps://pbs.twimg.com/profile_images/770491761412173826/ZUeIa4tw_400x400.jpg"
-  }
+    firstName: 'Commenter First Name',
+    lastName: 'Commenter Last Name',
+    photoUrl:
+      'url@url.comhttps://pbs.twimg.com/profile_images/770491761412173826/ZUeIa4tw_400x400.jpg',
+  },
 };
 const projectId = 1;
 const idx = 1;
 
-describe("Smoke test", () => {
+describe('Smoke test', () => {
   test('it renders without crashing', () => {
     render(
       <MemoryRouter>
         <UserProvider>
-          <Comment 
-            comment={comment}
-            projectId={projectId}
-            idx={idx}
-          />
+          <Comment comment={comment} projectId={projectId} idx={idx} />
         </UserProvider>
       </MemoryRouter>
     );
