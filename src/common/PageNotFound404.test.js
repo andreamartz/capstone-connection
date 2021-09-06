d79d00735b6be0085709d2c8f9ab9a7812b5341e
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 16:28:28 -0500

    Clean up code formatting

--- src/common/PageNotFound404.test.js
+++ src/common/PageNotFound404.test.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import PageNotFound404 from "./PageNotFound404";
-import { MemoryRouter } from "react-router";
+import React from 'react';
+import { render } from '@testing-library/react';
+import PageNotFound404 from './PageNotFound404';
+import { MemoryRouter } from 'react-router';
 
-describe("Smoke test", () => {
+describe('Smoke test', () => {
   test('it renders without crashing', () => {
-    render(<PageNotFound404 />)
+    render(<PageNotFound404 />);
   });
 });
