1fe2db21c00b80693e6164c0042bf2908d415671
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 21:09:36 -0500

    Clean up code formatting

--- src/comments/CommentForm.js
+++ src/comments/CommentForm.js
@@ -12,108 +12,103 @@ import UserContext from '../auth/UserContext';
 import CapConApi from '../api/api';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-    paddingLeft: theme.spacing(4),
-    paddingRight: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    height: 'auto',
-    width: '40rem',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.warning.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  button: {
-    margin: theme.spacing(2),
-  },
-  textField: {
-    marginBottom: theme.spacing(2),
-  },
+	paper: {
+		marginTop: theme.spacing(8),
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		paddingTop: theme.spacing(8),
+		paddingBottom: theme.spacing(8),
+		paddingLeft: theme.spacing(4),
+		paddingRight: theme.spacing(4),
+		display: 'flex',
+		flexDirection: 'column',
+		height: 'auto',
+		width: '40rem',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.warning.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
+	button: {
+		margin: theme.spacing(2),
+	},
+	textField: {
+		marginBottom: theme.spacing(2),
+	},
 }));
 
 const CommentForm = ({ projectId }) => {
-  const { currentUser } = useContext(UserContext);
-  const INITIAL_STATE_FORM_DATA = {
-    projectId: projectId,
-    commenterId: currentUser.id,
-    comment: '',
-  };
-  const [formData, setFormData] = useState(INITIAL_STATE_FORM_DATA);
-  const [formErrors, setFormErrors] = useState([]);
-  const classes = useStyles();
-  const history = useHistory();
+	const { currentUser } = useContext(UserContext);
+	const INITIAL_STATE_FORM_DATA = {
+		projectId: projectId,
+		commenterId: currentUser.id,
+		comment: '',
+	};
+	const [formData, setFormData] = useState(INITIAL_STATE_FORM_DATA);
+	const [formErrors, setFormErrors] = useState([]);
+	const classes = useStyles();
+	const history = useHistory();
 
-  console.debug('CommentForm', 'formData=', formData, 'formErrors', formErrors);
+	/** Update form data field */
+	const handleChange = (evt) => {
+		const { name, value } = evt.target;
+		setFormData((data) => ({ ...data, [name]: value }));
+	};
 
-  /** Update form data field */
-  const handleChange = (evt) => {
-    const { name, value } = evt.target;
-    setFormData((data) => ({ ...data, [name]: value }));
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+		const result = await CapConApi.addComment(formData);
 
-    // const comment = {...formData, comment};
+		if (result.id) {
+			history.push('/projects');
+		} else {
+			setFormErrors(result.error);
+		}
+	};
 
-    const result = await CapConApi.addComment(formData);
+	return (
+		<Paper className={classes.paper} elevation={3} component="main">
+			<Avatar className={classes.avatar}>
+				<FeedbackOutlinedIcon />
+			</Avatar>
+			<Box py={4}>
+				<Typography component="h1" variant="h5">
+					Give Feedback About This Capstone
+				</Typography>
+			</Box>
 
-    if (result.id) {
-      console.log('RESULT OF NEW COMMENT SUBMISSION: ', result);
-      history.push('/projects');
-    } else {
-      setFormErrors(result.error);
-    }
-  };
+			<form onSubmit={handleSubmit} className={classes.form}>
+				<TextField
+					className={classes.textField}
+					type="text"
+					name="comment"
+					label="Constructive feedback about project"
+					variant="outlined"
+					value={formData.comment}
+					multiline
+					rows={6}
+					fullWidth
+					required
+					onChange={handleChange}
+				/>
 
-  return (
-    <Paper className={classes.paper} elevation={3} component="main">
-      <Avatar className={classes.avatar}>
-        <FeedbackOutlinedIcon />
-      </Avatar>
-      <Box py={4}>
-        <Typography component="h1" variant="h5">
-          Give Feedback About This Capstone
-        </Typography>
-      </Box>
-
-      <form onSubmit={handleSubmit} className={classes.form}>
-        <TextField
-          className={classes.textField}
-          type="text"
-          name="comment"
-          label="Constructive feedback about project"
-          variant="outlined"
-          value={formData.comment}
-          multiline
-          rows={6}
-          fullWidth
-          required
-          onChange={handleChange}
-        />
-
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          type="submit"
-        >
-          Submit
-        </Button>
-      </form>
-    </Paper>
-  );
+				<Button
+					className={classes.button}
+					variant="contained"
+					color="primary"
+					type="submit"
+				>
+					Submit
+				</Button>
+			</form>
+		</Paper>
+	);
 };
 
 export default CommentForm;
