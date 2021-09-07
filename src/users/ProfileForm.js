552499baee712c6e85c79eb2e9d778bf5df07407
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-07 10:22:11 -0500

    Improve code formatting

--- src/users/ProfileForm.js
+++ src/users/ProfileForm.js
@@ -13,106 +13,102 @@ import AlertDisplay from '../common/AlertDisplay';
 import UserContext from '../auth/UserContext';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(16),
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-    paddingLeft: theme.spacing(4),
-    paddingRight: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'column',
-    height: 'auto',
-    width: '40rem',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    height: theme.spacing(30),
-  },
-  icon: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.warning.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  // submit: {
-  //   margin: theme.spacing(3, 0, 2),
-  // }
+	paper: {
+		marginTop: theme.spacing(16),
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		paddingTop: theme.spacing(8),
+		paddingBottom: theme.spacing(8),
+		paddingLeft: theme.spacing(4),
+		paddingRight: theme.spacing(4),
+		display: 'flex',
+		flexDirection: 'column',
+		height: 'auto',
+		width: '40rem',
+		alignItems: 'center',
+	},
+	avatar: {
+		margin: theme.spacing(1),
+		height: theme.spacing(30),
+	},
+	icon: {
+		margin: theme.spacing(1),
+		backgroundColor: theme.palette.warning.main,
+	},
+	form: {
+		width: '100%', // Fix IE 11 issue.
+		marginTop: theme.spacing(1),
+	},
 }));
 
 const ProfileForm = () => {
-  const { currentUser } = useContext(UserContext);
-  const [formData, setFormData] = useState({
-    password: '',
-    firstName: currentUser.firstName,
-    lastName: currentUser.lastName,
-    email: currentUser.email,
-    bio: currentUser.bio,
-    portfolioUrl: currentUser.portfolioUrl,
-    gitHubUrl: currentUser.gitHubUrl,
-  });
-  const [formErrors, setFormErrors] = useState([]);
+	const { currentUser } = useContext(UserContext);
+	const [formData, setFormData] = useState({
+		password: '',
+		firstName: currentUser.firstName,
+		lastName: currentUser.lastName,
+		email: currentUser.email,
+		bio: currentUser.bio,
+		portfolioUrl: currentUser.portfolioUrl,
+		gitHubUrl: currentUser.gitHubUrl,
+	});
+	const [formErrors, setFormErrors] = useState([]);
 
-  const classes = useStyles();
-  const history = useHistory();
-  // const fileInputRef = useRef();
-  const { username } = useParams();
+	const classes = useStyles();
+	const history = useHistory();
+	// const fileInputRef = useRef();
+	const { username } = useParams();
 
-  console.debug('ProfileForm', 'formData=', formData, 'formErrors', formErrors);
+	console.debug('ProfileForm', 'formData=', formData, 'formErrors', formErrors);
 
-  const handleSubmit = async (evt) => {
-    evt.preventDefault();
+	const handleSubmit = async (evt) => {
+		evt.preventDefault();
 
-    const profileData = {
-      password: formData.password,
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      email: formData.email,
-      bio: formData.bio,
-      portfolioUrl: formData.portfolioUrl,
-      gitHubUrl: formData.gitHubUrl,
-    };
+		const profileData = {
+			password: formData.password,
+			firstName: formData.firstName,
+			lastName: formData.lastName,
+			email: formData.email,
+			bio: formData.bio,
+			portfolioUrl: formData.portfolioUrl,
+			gitHubUrl: formData.gitHubUrl,
+		};
 
-    let updatedUser;
+		let updatedUser;
 
-    try {
-      updatedUser = await CapConApi.updateProfile(currentUser.id, profileData);
-      console.log('UPDATEDUSER: ', updatedUser);
-      history.push(`/users/${currentUser.id}`);
-    } catch (errors) {
-      setFormErrors(errors);
-      return;
-    }
+		try {
+			updatedUser = await CapConApi.updateProfile(currentUser.id, profileData);
+			history.push(`/users/${currentUser.id}`);
+		} catch (errors) {
+			setFormErrors(errors);
+			return;
+		}
 
-    setFormData((data) => ({ ...data }));
-  };
+		setFormData((data) => ({ ...data }));
+	};
 
-  /** Update form data field */
-  function handleChange(evt) {
-    const { name, value } = evt.target;
-    setFormData((data) => ({ ...data, [name]: value }));
-  }
-  return (
-    <Paper className={classes.paper} elevation={5} component="main">
-      <Avatar className={classes.icon}>
-        <EditOutlinedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Profile
-      </Typography>
-      <Box my={2} id="form--image-container">
-        <img
-          src={currentUser.photoUrl}
-          alt="current user"
-          className={classes.avatar}
-        />
-      </Box>
-      <form className={classes.form} onSubmit={handleSubmit}>
-        {/* <TextField
+	/** Update form data field */
+	function handleChange(evt) {
+		const { name, value } = evt.target;
+		setFormData((data) => ({ ...data, [name]: value }));
+	}
+	return (
+		<Paper className={classes.paper} elevation={5} component="main">
+			<Avatar className={classes.icon}>
+				<EditOutlinedIcon />
+			</Avatar>
+			<Typography component="h1" variant="h5">
+				Profile
+			</Typography>
+			<Box my={2} id="form--image-container">
+				<img
+					src={currentUser.photoUrl}
+					alt="current user"
+					className={classes.avatar}
+				/>
+			</Box>
+			<form className={classes.form} onSubmit={handleSubmit}>
+				{/* <TextField
           variant="outlined"
           margin="normal"
           required
@@ -126,107 +122,107 @@ const ProfileForm = () => {
           autoComplete="username"
           autoFocus
         /> */}
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="password"
-          label="Password"
-          name="password"
-          onChange={handleChange}
-          type="password"
-          value={formData.password}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="firstName"
-          label="First name"
-          name="firstName"
-          onChange={handleChange}
-          type="firstName"
-          value={formData.firstName}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          required
-          fullWidth
-          id="lastName"
-          label="Last name"
-          name="lastName"
-          onChange={handleChange}
-          type="lastName"
-          value={formData.lastName}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="email"
-          label="Email address"
-          name="email"
-          onChange={handleChange}
-          type="email"
-          value={formData.email}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="bio"
-          label="Bio"
-          name="bio"
-          onChange={handleChange}
-          type="bio"
-          value={formData.bio}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="portfolioUrl"
-          label="URL of portfolio site"
-          name="portfolioUrl"
-          onChange={handleChange}
-          type="portfolioUrl"
-          value={formData.portfolioUrl}
-        />
-        <TextField
-          variant="outlined"
-          margin="normal"
-          fullWidth
-          id="gitHubUrl"
-          label="URL of GitHub profile"
-          name="gitHubUrl"
-          onChange={handleChange}
-          type="gitHubUrl"
-          value={formData.gitHubUrl}
-        />
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="password"
+					label="Password"
+					name="password"
+					onChange={handleChange}
+					type="password"
+					value={formData.password}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="firstName"
+					label="First name"
+					name="firstName"
+					onChange={handleChange}
+					type="firstName"
+					value={formData.firstName}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					required
+					fullWidth
+					id="lastName"
+					label="Last name"
+					name="lastName"
+					onChange={handleChange}
+					type="lastName"
+					value={formData.lastName}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					fullWidth
+					id="email"
+					label="Email address"
+					name="email"
+					onChange={handleChange}
+					type="email"
+					value={formData.email}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					fullWidth
+					id="bio"
+					label="Bio"
+					name="bio"
+					onChange={handleChange}
+					type="bio"
+					value={formData.bio}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					fullWidth
+					id="portfolioUrl"
+					label="URL of portfolio site"
+					name="portfolioUrl"
+					onChange={handleChange}
+					type="portfolioUrl"
+					value={formData.portfolioUrl}
+				/>
+				<TextField
+					variant="outlined"
+					margin="normal"
+					fullWidth
+					id="gitHubUrl"
+					label="URL of GitHub profile"
+					name="gitHubUrl"
+					onChange={handleChange}
+					type="gitHubUrl"
+					value={formData.gitHubUrl}
+				/>
 
-        {/* {formErrors.length
+				{/* {formErrors.length
           ? <Alert type="danger">This is an error alert — check it out!</Alert>
           : null
         } */}
-        {formErrors.length ? (
-          <AlertDisplay severity="error" messages={formErrors} />
-        ) : null}
-        <Button
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="primary"
-          className={classes.submit}
-          size="large"
-        >
-          Submit
-        </Button>
-      </form>
-    </Paper>
-  );
+				{formErrors.length ? (
+					<AlertDisplay severity="error" messages={formErrors} />
+				) : null}
+				<Button
+					type="submit"
+					fullWidth
+					variant="contained"
+					color="primary"
+					className={classes.submit}
+					size="large"
+				>
+					Submit
+				</Button>
+			</form>
+		</Paper>
+	);
 };
 
 export default ProfileForm;
