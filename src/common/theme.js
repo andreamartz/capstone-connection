696b8992ba671dc0a174af17fe5800ad446cd379
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-07 10:09:10 -0500

    Improve code formatting

--- src/common/theme.js
+++ src/common/theme.js
@@ -3,33 +3,32 @@ import { createTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#3949ab',
-    },
-    secondary: {
-      main: '#ce93d8',
-    },
-    error: {
-      main: '#ff4025',
-    },
-    warning: {
-      main: '#ff9c2a',
-    },
-    success: {
-      main: '#006400',
-    },
-  },
-  status: {
-    danger: 'orange',
-  },
-  typography: {
-    // fontFamily: 'Roboto Mono, monospace',
-    fontFamily: 'Montserrat, sans-serif',
-    fontWeightLight: 400,
-    fontWeightRegular: 500,
-    fontWeightBold: 600,
-  },
+	palette: {
+		primary: {
+			main: '#3949ab',
+		},
+		secondary: {
+			main: '#ce93d8',
+		},
+		error: {
+			main: '#ff4025',
+		},
+		warning: {
+			main: '#ff9c2a',
+		},
+		success: {
+			main: '#006400',
+		},
+	},
+	status: {
+		danger: 'orange',
+	},
+	typography: {
+		fontFamily: 'Montserrat, sans-serif',
+		fontWeightLight: 400,
+		fontWeightRegular: 500,
+		fontWeightBold: 600,
+	},
 });
 
 export default theme;
