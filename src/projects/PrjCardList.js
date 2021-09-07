59ee2b366caf43b01439f3fcdc52030aba2ac973
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-07 10:18:39 -0500

    Improve code formatting

--- src/projects/PrjCardList.js
+++ src/projects/PrjCardList.js
@@ -14,13 +14,13 @@ import { toggleLikeProject } from '../utils';
 import './PrjCardList.css';
 
 const useStyles = makeStyles((theme) => ({
-  project: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-  },
-  searchForm: {
-    flexGrow: '1',
-  },
+	project: {
+		marginTop: theme.spacing(3),
+		marginBottom: theme.spacing(3),
+	},
+	searchForm: {
+		flexGrow: '1',
+	},
 }));
 
 /** Show project cards.
@@ -32,36 +32,37 @@ const useStyles = makeStyles((theme) => ({
  */
 
 const PrjCardList = ({ userId }) => {
-  const { currentUser } = useContext(UserContext);
-  const [projects, setProjects] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [sortVariable, setSortVariable] = useState('');
-  const [showSearchAndSort, setShowSearchAndSort] = useState(false);
+	const { currentUser } = useContext(UserContext);
+	const [projects, setProjects] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+	const [sortVariable, setSortVariable] = useState('');
+	const [showSearchAndSort, setShowSearchAndSort] = useState(false);
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  useEffect(() => {
-    async function getAllProjectsOnMount() {
-      const projects = userId
-        ? await CapConApi.getUserProjects(userId)
-        : await CapConApi.getProjects(searchTerm, sortVariable);
-      setProjects(projects);
-      if (userId) {
-        setShowSearchAndSort(false);
-      } else {
-        setShowSearchAndSort(true);
-      }
-    }
-    getAllProjectsOnMount();
-  }, [userId, sortVariable, searchTerm]);
 
-  // Breakpoints
-  const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-  const flexDirection = isSmallScreen ? 'column' : 'row';
-  const justifyContent = isSmallScreen ? 'center' : 'space-between';
-  const alignItems = isSmallScreen ? 'flex-start' : 'space-between';
-  const marginBottom = isSmallScreen ? 10 : 1;
+	useEffect(() => {
+		async function getAllProjectsOnMount() {
+			const projects = userId
+				? await CapConApi.getUserProjects(userId)
+				: await CapConApi.getProjects(searchTerm, sortVariable);
+			setProjects(projects);
+			if (userId) {
+				setShowSearchAndSort(false);
+			} else {
+				setShowSearchAndSort(true);
+			}
+		}
+		getAllProjectsOnMount();
+	}, [userId, sortVariable, searchTerm]);
+
+	// Breakpoints
+	const theme = useTheme();
+	const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+	const flexDirection = isSmallScreen ? 'column' : 'row';
+	const justifyContent = isSmallScreen ? 'center' : 'space-between';
+	const alignItems = isSmallScreen ? 'flex-start' : 'space-between';
+	const marginBottom = isSmallScreen ? 10 : 1;
 
   // const search = async () => {
   //   const projects = await CapConApi.getProjects(searchTerm, sortVariable);
@@ -75,68 +76,56 @@ const PrjCardList = ({ userId }) => {
   //   setProjects(sortedProjects);
   // }
 
-  if (!projects) return <LoadingSpinner />;
+	if (!projects) return <LoadingSpinner />;
 
-  const breakpoints = {
-    default: 3, // default number of columns
-    960: 2, // at screen width of 1100px, reduce to 2 columns
-    600: 1,
-  };
+	const breakpoints = {
+		default: 3, // default number of columns
+		960: 2, // at screen width of 1100px, reduce to 2 columns
+		600: 1,
+	};
 
-  console.log('PROJECTS STATE: ', projects);
+	console.log('PROJECTS STATE: ', projects);
 
-  return (
-    <Container>
-      <Box
-        display="flex"
-        flexDirection={flexDirection}
-        justifyContent={justifyContent}
-        alignItems={alignItems}
-        mb={5}
-      >
-        {/* {showSearchAndSort && <SearchForm className={classes.searchForm} search={search} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />}
-        <Box mb={marginBottom}>
-          {showSearchAndSort && <SortForm sort={sort} sortVariable={sortVariable} setSortVariable={setSortVariable}/>} */}
+			>
+				{showSearchAndSort && (
+					<SearchForm
+						className={classes.searchForm}
+						searchTerm={searchTerm}
+						setSearchTerm={setSearchTerm}
+					/>
+				)}
+				<Box mb={marginBottom}>
+					{showSearchAndSort && (
+						<SortForm
+							sortVariable={sortVariable}
+							setSortVariable={setSortVariable}
+						/>
+					)}
+				</Box>
+			</Box>
 
-        {showSearchAndSort && (
-          <SearchForm
-            className={classes.searchForm}
-            searchTerm={searchTerm}
-            setSearchTerm={setSearchTerm}
-          />
-        )}
-        <Box mb={marginBottom}>
-          {showSearchAndSort && (
-            <SortForm
-              sortVariable={sortVariable}
-              setSortVariable={setSortVariable}
-            />
-          )}
-        </Box>
-      </Box>
-
-      {projects.length ? (
-        <Masonry
-          breakpointCols={breakpoints}
-          className="masonry-grid"
-          columnClassName="masonry-grid_column"
-        >
-          {projects.map((p, idx) => (
-            <div key={p.id} className={classes.project}>
-              <PrjCardVert
-                project={p}
-                toggleLike={() => {
-                  toggleLikeProject(idx, currentUser, projects, setProjects);
-                }}
-              />
-            </div>
-          ))}
-        </Masonry>
-      ) : (
-        <p>Sorry, no projects were found!</p>
-      )}
-    </Container>
-  );
+			{projects.length ? (
+				<Masonry
+					breakpointCols={breakpoints}
+					className="masonry-grid"
+					columnClassName="masonry-grid_column"
+				>
+					{projects.map((p, idx) => (
+						<div key={p.id} className={classes.project}>
+							<PrjCardVert
+								project={p}
+								toggleLike={() => {
+									toggleLikeProject(idx, currentUser, projects, setProjects);
+								}}
+							/>
+						</div>
+					))}
+				</Masonry>
+			) : (
+				<p>Sorry, no projects were found!</p>
+			)}
+		</Container>
+	);
 };
 
 export default PrjCardList;
