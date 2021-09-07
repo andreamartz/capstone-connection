c691cea20c54e8dca484571866a760e41e08e9a7
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-07 10:08:41 -0500

    Improve code formatting

--- src/comments/CommentList.js
+++ src/comments/CommentList.js
@@ -4,28 +4,27 @@ import { List } from '@material-ui/core';
 import Comment from './Comment';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    backgroundColor: theme.palette.background.paper,
-  },
+	root: {
+		width: '100%',
+		backgroundColor: theme.palette.background.paper,
+	},
 }));
 
 const CommentList = ({ comments, projectId }) => {
-  console.log('CommentList', 'comments= ', comments);
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <List className={classes.root}>
-      {comments.map((comment, idx) => (
-        <Comment
-          key={comment.id}
-          projectId={projectId}
-          comment={comment}
-          idx={idx}
-        />
-      ))}
-    </List>
-  );
+	return (
+		<List className={classes.root}>
+			{comments.map((comment, idx) => (
+				<Comment
+					key={comment.id}
+					projectId={projectId}
+					comment={comment}
+					idx={idx}
+				/>
+			))}
+		</List>
+	);
 };
 
 export default CommentList;
