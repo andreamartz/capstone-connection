c54906025438f68c6efdf9e10156a8025d0dc24c
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-06 21:08:14 -0500

    Clean up code formatting

--- src/projects/SortForm.js
+++ src/projects/SortForm.js
@@ -6,61 +6,60 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    display: 'block',
-    marginTop: theme.spacing(2),
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-  select: {
-    width: theme.spacing(20),
-  },
+	button: {
+		display: 'block',
+		marginTop: theme.spacing(2),
+	},
+	formControl: {
+		margin: theme.spacing(1),
+		minWidth: 120,
+	},
+	select: {
+		width: theme.spacing(20),
+	},
 }));
 
 const SortForm = ({ sort, sortVariable, setSortVariable }) => {
-  const [open, setOpen] = useState(false);
-  const classes = useStyles();
+	const [open, setOpen] = useState(false);
+	const classes = useStyles();
 
-  const handleChange = async (event) => {
-    setSortVariable((sortVariable) => event.target.value);
-    // await sort(sortVariable);
-  };
+	const handleChange = async (event) => {
+		setSortVariable((sortVariable) => event.target.value);
+	};
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+	const handleOpen = () => {
+		setOpen(true);
+	};
 
-  return (
-    <FormControl className={classes.formControl}>
-      <InputLabel id="sortVariable">Sort projects by</InputLabel>
-      <Select
-        labelId="sort variable"
-        id="sortVariableSelect"
-        open={open}
-        onClose={handleClose}
-        onOpen={handleOpen}
-        value={sortVariable}
-        onChange={handleChange}
-        className={classes.select}
-      >
-        <MenuItem value="">
-          <em>None</em>
-        </MenuItem>
-        <MenuItem value={'newest'} onChange={handleChange}>
-          Newest
-        </MenuItem>
-        <MenuItem value={'most likes'} onChange={handleChange}>
-          Most Likes
-        </MenuItem>
-      </Select>
-    </FormControl>
-  );
+	return (
+		<FormControl className={classes.formControl}>
+			<InputLabel id="sortVariable">Sort projects by</InputLabel>
+			<Select
+				labelId="sort variable"
+				id="sortVariableSelect"
+				open={open}
+				onClose={handleClose}
+				onOpen={handleOpen}
+				value={sortVariable}
+				onChange={handleChange}
+				className={classes.select}
+			>
+				<MenuItem value="">
+					<em>None</em>
+				</MenuItem>
+				<MenuItem value={'newest'} onChange={handleChange}>
+					Newest
+				</MenuItem>
+				<MenuItem value={'most likes'} onChange={handleChange}>
+					Most Likes
+				</MenuItem>
+			</Select>
+		</FormControl>
+	);
 };
 
 export default SortForm;
