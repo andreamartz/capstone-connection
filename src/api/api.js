2d1be260b7fbbdf76c2c5951cd70dab52d6a5b92
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-07 10:07:37 -0500

    Improve code formatting

--- src/api/api.js
+++ src/api/api.js
@@ -8,152 +8,148 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
  */
 
 class CapConApi {
-  // The token for interacting with the API will be stored here.
-  static token;
+	// The token for interacting with the API will be stored here.
+	static token;
 
-  static async request(endpoint, data = {}, method = 'get') {
-    console.debug('API call: ', endpoint, data, method);
+	static async request(endpoint, data = {}, method = 'get') {
+		console.debug('API call: ', endpoint, data, method);
 
-    const url = `${BASE_URL}/${endpoint}`;
-    const headers = { Authorization: `Bearer ${CapConApi.token}` };
-    const params = method === 'get' ? data : {};
+		const url = `${BASE_URL}/${endpoint}`;
+		const headers = { Authorization: `Bearer ${CapConApi.token}` };
+		const params = method === 'get' ? data : {};
 
-    try {
-      return (await axios({ url, method, data, params, headers })).data;
-    } catch (err) {
-      console.error('ERR: ', err);
-      console.error('API Error:', err.response);
-      // const message = err.response.data.error.message;
-      const message = err.message;
-      console.log('MESSAGE: ', message);
-      // throw Array.isArray(message) ? message : [message];
-    }
-  }
+		try {
+			return (await axios({ url, method, data, params, headers })).data;
+		} catch (err) {
+			console.error('ERR: ', err);
+			console.error('API Error:', err.response);
+			// const message = err.response.data.error.message;
+			const message = err.message;
+			// throw Array.isArray(message) ? message : [message];
+		}
+	}
 
-  // ------------------------------
-  // Individual API routes
-  // ------------------------------
+	// ------------------------------
+	// Individual API routes
+	// ------------------------------
 
-  /** Get a user. */
+	/** Get a user. */
 
-  static async getUser(id) {
-    const res = await this.request(`users/${id}`);
+	static async getUser(id) {
+		const res = await this.request(`users/${id}`);
 
-    return res?.user;
-  }
+		return res?.user;
+	}
 
-  /** Get a user's projects */
+	/** Get a user's projects */
 
-  static async getUserProjects(userId) {
-    const res = await this.request(`users/${userId}/projects`);
-    return res.projects;
-  }
+	static async getUserProjects(userId) {
+		const res = await this.request(`users/${userId}/projects`);
+		return res.projects;
+	}
 
-  /** Get a list of projects */
+	/** Get a list of projects */
 
-  static async getProjects(tagText, sortVariable) {
-    const res = await this.request('projects', { tagText, sortVariable });
+	static async getProjects(tagText, sortVariable) {
+		const res = await this.request('projects', { tagText, sortVariable });
 
-    return res.projects;
-  }
+		return res.projects;
+	}
 
-  /** Get a project */
+	/** Get a project */
 
-  static async getProject(id) {
-    const res = await this.request(`projects/${id}`);
-    return res?.project;
-  }
+	static async getProject(id) {
+		const res = await this.request(`projects/${id}`);
+		return res?.project;
+	}
 
-  /** Add a new project */
+	/** Add a new project */
 
-  static async addProject(data) {
-    console.log('DATA SENT: ', data);
-    let res = await this.request('projects', data, 'post');
-    console.log('RES from CapConApi: ', res);
-    return res.project;
-  }
+	static async addProject(data) {
+		let res = await this.request('projects', data, 'post');
+		return res.project;
+	}
 
-  /** Like a project */
+	/** Like a project */
 
-  static async addProjectLike(data) {
-    const { projectId } = data;
-    const res = await this.request(`projects/${projectId}/likes`, data, 'post');
-    return res.projectLike;
-  }
+	static async addProjectLike(data) {
+		const { projectId } = data;
+		const res = await this.request(`projects/${projectId}/likes`, data, 'post');
+		return res.projectLike;
+	}
 
-  /** Unlike a project */
+	/** Unlike a project */
 
-  static async removeProjectLike(data) {
-    const { projectId, currentUsersLikeId, userId } = data;
-    const res = await this.request(
-      `projects/${projectId}/likes/${currentUsersLikeId}`,
-      data,
-      'delete'
-    );
-    return res.deleted;
-  }
+	static async removeProjectLike(data) {
+		const { projectId, currentUsersLikeId, userId } = data;
+		const res = await this.request(
+			`projects/${projectId}/likes/${currentUsersLikeId}`,
+			data,
+			'delete',
+		);
+		return res.deleted;
+	}
 
-  /** Add a new tag
-   * data = { text }
-   */
-  static async addTag(data) {
-    const res = await this.request(`tags`, data, 'post');
-    return res.tagText;
-  }
+	/** Add a new tag
+	 * data = { text }
+	 */
+	static async addTag(data) {
+		const res = await this.request(`tags`, data, 'post');
+		return res.tagText;
+	}
 
-  /** Get a list of tags */
+	/** Get a list of tags */
 
-  static async getTags() {
-    const res = await this.request('tags');
-    return res.tags;
-  }
+	static async getTags() {
+		const res = await this.request('tags');
+		return res.tags;
+	}
 
-  /** Add a comment to a project */
+	/** Add a comment to a project */
 
-  static async addComment(data) {
-    const res = await this.request('project_comments', data, 'post');
-    return res.comment;
-  }
+	static async addComment(data) {
+		const res = await this.request('project_comments', data, 'post');
+		return res.comment;
+	}
 
-  /** Edit a comment about a project */
+	/** Edit a comment about a project */
 
-  static async updateComment(commentId, data) {
-    const res = await this.request(
-      `project_comments/${commentId}`,
-      data,
-      'patch'
-    );
-    return res.comment;
-  }
+	static async updateComment(commentId, data) {
+		const res = await this.request(
+			`project_comments/${commentId}`,
+			data,
+			'patch',
+		);
+		return res.comment;
+	}
 
-  /** Get token for login from username, password. */
+	/** Get token for login from username, password. */
 
-  static async login(data) {
-    const res = await this.request(`auth/token`, data, 'post');
-    return res.token;
-  }
+	static async login(data) {
+		const res = await this.request(`auth/token`, data, 'post');
+		return res.token;
+	}
 
-  /** Signup for site. */
+	/** Signup for site. */
 
-  static async signup(data) {
-    const res = await this.request(`auth/register`, data, 'post');
-    return res.token;
-  }
+	static async signup(data) {
+		const res = await this.request(`auth/register`, data, 'post');
+		return res.token;
+	}
 
-  /** Register demo user. */
+	/** Register demo user. */
 
-  static async signupDemo() {
-    console.log('CALLED CAPCONAPI.signupDemo');
-    const res = await this.request(`auth/register/demo`);
-    return res.token;
-  }
+	static async signupDemo() {
+		const res = await this.request(`auth/register/demo`);
+		return res.token;
+	}
 
-  /** Update user profile */
+	/** Update user profile */
 
-  static async updateProfile(userId, data) {
-    const res = await this.request(`users/${userId}`, data, 'patch');
-    return res.user;
-  }
+	static async updateProfile(userId, data) {
+		const res = await this.request(`users/${userId}`, data, 'patch');
+		return res.user;
+	}
 }
 
 export default CapConApi;
