0b8ae660d790d25c8ac592d94bf16eebf29587bb
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-15 14:15:41 -0500

    Clean up code formatting

--- src/common/LoadingSpinner.js
+++ src/common/LoadingSpinner.js
@@ -12,20 +12,20 @@ import { makeStyles } from '@material-ui/core/styles';
  */
 
 const useStyles = makeStyles((theme) => ({
-  message: {
-    margin: '200px auto 0 auto',
-    color: 'tomato',
-    fontSize: theme.spacing(3),
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
+	message: {
+		margin: '200px auto 0 auto',
+		color: 'tomato',
+		fontSize: theme.spacing(3),
+		fontWeight: 'bold',
+		textAlign: 'center',
+	},
 }));
 
 /** Loading message used by components that fetch API data. */
 
 function LoadingSpinner() {
-  const classes = useStyles();
-  return <div className={classes.message}>Loading ...</div>;
+	const classes = useStyles();
+	return <div className={classes.message}>Loading ...</div>;
 }
 
 export default LoadingSpinner;
