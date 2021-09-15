3dab0f0a1ddec70e877b99af7d6ff5a451e78d87
andreamartz <14165788+andreamartz@users.noreply.github.com>
2021-09-15 11:55:19 -0500

    Improve code formatting

--- src/comments/Comment.js
+++ src/comments/Comment.js
@@ -4,94 +4,93 @@ import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/Edit';
 import {
-  ListItem,
-  Divider,
-  ListItemText,
-  ListItemAvatar,
-  Avatar,
-  Typography,
+	ListItem,
+	Divider,
+	ListItemText,
+	ListItemAvatar,
+	Avatar,
+	Typography,
 } from '@material-ui/core';
 import UserContext from '../auth/UserContext';
 import EditCommentForm from './EditCommentForm';
 import './Comment.css';
 
 const useStyles = makeStyles((theme) => ({
-  fonts: {
-    fontWeight: 'bold',
-  },
-  inline: {
-    display: 'inline',
-  },
-  button: {
-    backgroundColor: '#ff9c2a',
-  },
+	fonts: {
+		fontWeight: 'bold',
+	},
+	inline: {
+		display: 'inline',
+	},
+	button: {
+		backgroundColor: '#ff9c2a',
+	},
 }));
 
 const Comment = ({ comment, projectId, idx }) => {
-  console.debug('Comment ', 'COMMENT: ', comment);
-  const [formVisible, setFormVisible] = useState(false);
-  const [commentState, setCommentState] = useState(comment);
-  const { currentUser } = useContext(UserContext);
-  const classes = useStyles();
+	const [formVisible, setFormVisible] = useState(false);
+	const [commentState, setCommentState] = useState(comment);
+	const { currentUser } = useContext(UserContext);
+	const classes = useStyles();
 
-  const { id, commenter } = comment;
-  const { firstName, lastName, photoUrl } = commenter;
+	const { id, commenter } = comment;
+	const { firstName, lastName, photoUrl } = commenter;
 
-  const toggleForm = () => {
-    setFormVisible(!formVisible);
-  };
+	const toggleForm = () => {
+		setFormVisible(!formVisible);
+	};
 
-  return (
-    <>
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <ListItem key={id} alignItems="flex-start">
-          <ListItemAvatar>
-            <Avatar alt="commenter" src={photoUrl} />
-          </ListItemAvatar>
-          <ListItemText
-            primary={
-              <Typography className={classes.fonts}>
-                {firstName} {lastName}
-              </Typography>
-            }
-            secondary={
-              <Typography
-                component="span"
-                variant="body2"
-                className={classes.inline}
-                color="textPrimary"
-              >
-                {commentState.comment}
-              </Typography>
-            }
-          />
-        </ListItem>
-        {currentUser.id === commenter.id ? (
-          <Button
-            variant="contained"
-            startIcon={<EditIcon />}
-            color="secondary"
-            className={classes.button}
-            onClick={toggleForm}
-          >
-            Edit
-          </Button>
-        ) : null}
-      </Box>
-      {formVisible && (
-        <Box>
-          <EditCommentForm
-            commentState={commentState}
-            projectId={projectId}
-            idx={idx}
-            setFormVisible={setFormVisible}
-            setCommentState={setCommentState}
-          />
-        </Box>
-      )}
-      <Divider />
-    </>
-  );
+	return (
+		<>
+			<Box display="flex" justifyContent="space-between" alignItems="center">
+				<ListItem key={id} alignItems="flex-start">
+					<ListItemAvatar>
+						<Avatar alt="commenter" src={photoUrl} />
+					</ListItemAvatar>
+					<ListItemText
+						primary={
+							<Typography className={classes.fonts}>
+								{firstName} {lastName}
+							</Typography>
+						}
+						secondary={
+							<Typography
+								component="span"
+								variant="body2"
+								className={classes.inline}
+								color="textPrimary"
+							>
+								{commentState.comment}
+							</Typography>
+						}
+					/>
+				</ListItem>
+				{currentUser.id === commenter.id ? (
+					<Button
+						variant="contained"
+						startIcon={<EditIcon />}
+						color="secondary"
+						className={classes.button}
+						onClick={toggleForm}
+					>
+						Edit
+					</Button>
+				) : null}
+			</Box>
+			{formVisible && (
+				<Box>
+					<EditCommentForm
+						commentState={commentState}
+						projectId={projectId}
+						idx={idx}
+						setFormVisible={setFormVisible}
+						setCommentState={setCommentState}
+					/>
+				</Box>
+			)}
+			<Divider />
+		</>
+	);
 };
 
 export default Comment;
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -16,24 +16,22 @@ import { useState, useEffect } from 'react';
  */
 
 function useLocalStorage(key, firstValue = null) {
-  const initialValue = localStorage.getItem(key) || firstValue;
+	const initialValue = localStorage.getItem(key) || firstValue;
 
-  const [item, setItem] = useState(initialValue);
+	const [item, setItem] = useState(initialValue);
 
-  useEffect(
-    function setKeyInLocalStorage() {
-      console.debug('hooks useLocalStorage useEffect', 'item=', item);
+	useEffect(
+		function setKeyInLocalStorage() {
+			if (item === null) {
+				localStorage.removeItem(key);
+			} else {
+				localStorage.setItem(key, item);
+			}
+		},
+		[key, item],
+	);
 
-      if (item === null) {
-        localStorage.removeItem(key);
-      } else {
-        localStorage.setItem(key, item);
-      }
-    },
-    [key, item]
-  );
-
-  return [item, setItem];
+	return [item, setItem];
 }
 
 export default useLocalStorage;
